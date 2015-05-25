7a3d8ae94ca9530c8178e562730e35537633c80c
KeleiAzz <kgong@ncsu.edu>
2015-05-24 22:31:32 -0400

    remove some commented lines

--- lists/views.py
+++ lists/views.py
@@ -1,28 +1,18 @@
 from django.shortcuts import redirect, render
-from django.http import HttpResponse
 from lists.models import Item, List
 from django.core.exceptions import ValidationError
 
 
 # Create your views here.
 def home_page(request):
-    # if request.method == 'POST':
-    #     return HttpResponse(request.POST['item_text'])
-    # if request.method == 'POST':
-    #     Item.objects.create(text=request.POST['item_text'])
-    #     return redirect('/lists/the-only-list-in-the-world/')
-
-    # items = Item.objects.all()
     return render(request, 'home.html')
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    # items = Item.objects.filter(list=list_)
     return render(request, 'list.html', {'list': list_})
 
 def new_list(request):
     list_ = List.objects.create()
-    # item = Item.objects.create(text=request.POST['item_text'], list=list_)
     item = Item(text=request.POST['item_text'], list=list_)
     try:
         item.full_clean()
